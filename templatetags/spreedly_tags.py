d52708bb760ec5664749b99a145ec76b09e06f94
Chad Glendenin <chad@glendenin.com>
2010-04-14 21:38:00 -0500

    don't crash if request.user.subscription doesn't exist.
    template tag now returns SPREEDLY_URL instead of throwing exception in
    that case.

--- templatetags/spreedly_tags.py
+++ templatetags/spreedly_tags.py
@@ -2,15 +2,20 @@ from django.conf import settings
 from django import template
 
 from spreedly.functions import subscription_url
+import spreedly.settings as spreedly_settings
+from spreedly.models import Subscription
 
 register = template.Library()
 
 @register.simple_tag
 def existing_plan_url(user):
-    return 'https://spreedly.com/%(site_name)s/subscriber_accounts/%(user_token)s' % {
-        'site_name': settings.SPREEDLY_SITE_NAME,
-        'user_token': user.subscription.token
-    }
+    try:
+        return 'https://spreedly.com/%(site_name)s/subscriber_accounts/%(user_token)s' % {
+            'site_name': settings.SPREEDLY_SITE_NAME,
+            'user_token': user.subscription.token
+        }
+    except Subscription.DoesNotExist:
+        return spreedly_settings.SPREEDLY_URL
 
 @register.simple_tag
 def new_plan_url(plan, user):
